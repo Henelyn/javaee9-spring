a94e7aa7219a62802daf1c4975349ff037c609ee
Henelyn <henelyn@gmail.com>
2022-01-29 14:08:34 +0200

    [change] - composition vs inheritance explanation

--- src/test/java/com/sda/javaee9spring/Javaee9SpringApplicationTests.java
+++ src/test/java/com/sda/javaee9spring/Javaee9SpringApplicationTests.java
@@ -10,4 +10,29 @@ class Javaee9SpringApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void dependencyInjectionFirstTrial() {
+        Toy furryTeddy = new Toy("furry Teddy");
+        Child john = new Child("John", furryTeddy);
+        // dependency injection is providing all required class dependencies during creation
+        // of object of that class
+        // furryTeddy (of type Toy) is dependency of john (of type Child)
+        //john (witch is of type Child) is dependency of jason (of type Parent)
+        // dependency is actually composition, so one item is made of another
+        Parent jason = new Parent("Jason", john);
+    }
 }
+//Toy is made of name
+record Toy(String name){}
+//Child is composed of: name and toy
+record Child(String name, Toy favoriteToy) { }
+// parent is made of: name and child
+record Parent(String name, Child child) {
+}
+
+class Animal{}
+class Mammal extends Animal {}
+class Tiger extends Mammal{}
+//Tiger is Mammal
+//Tiger is Animal
+//We cannot say: Parent is Child (parent don't extend Child)
