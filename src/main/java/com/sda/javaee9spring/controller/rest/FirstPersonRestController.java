3f5cb5c0b8208b957cda9d6e1cd67ca1aceb8c90
Henelyn <henelyn@gmail.com>
2022-02-12 13:38:54 +0200

    [add] - playing with ResponseEntity API continued

--- src/main/java/com/sda/javaee9spring/controller/rest/FirstPersonRestController.java
+++ src/main/java/com/sda/javaee9spring/controller/rest/FirstPersonRestController.java
@@ -2,6 +2,7 @@ package com.sda.javaee9spring.controller.rest;
 
 import com.sda.javaee9spring.entityOrModel.Person;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,5 +28,12 @@ public class FirstPersonRestController {
     @GetMapping("/custom")
     public ResponseEntity<Person> customResponseEntity(){
         return new ResponseEntity<>(new Person("Kate", "M", 10), HttpStatus.OK);
+    }
+
+    @GetMapping("/with-headers")
+    public ResponseEntity<Person> customResponseEntityWithHeaders(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Name", "henelyn");
+        return new ResponseEntity<>(new Person("Toomas", "Agu", 45), headers, HttpStatus.OK);
     }
 }
